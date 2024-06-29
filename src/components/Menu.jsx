2faa5233607169d757a84d35ab37861d4da46ea7
DonJuan <armvnov1ch@gmail.com>
2024-06-29 14:13:48 +0500

    kaspi link added

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -8,7 +8,7 @@ const Menu = () => {
                <a href="/correction">Коррекция</a>
                <a href="/equipment">Оборудование</a>
                <a href="/faq">FAQ</a>
-               <a id="donation" href="#">
+               <a id="donation" href="https://kaspi.kz/pay/OBalamaiAi?started_from=QR">
                   Пожертвовать
                </a>
             </div>
