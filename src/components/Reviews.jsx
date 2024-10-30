4aacf43bc189fd079c59701c6858dbc7b1f8d314
DonJuan <armvnov1ch@gmail.com>
2024-10-30 20:34:09 +0500

    reviews adaptive

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -17,21 +17,54 @@ const Carousel = ({ slides }) => {
    return (
       <div id="reviews">
          <div className="container">
-            <h3 id="title">Отзывы родителей</h3>
-            <div className="carousel">
-               <button id="arrows" onClick={prevSlide}>
-                  <img
-                     src={`${process.env.PUBLIC_URL}/img/prev.png`}
-                     alt=""
-                  />
-               </button>
-               <div className="slide">
-                  <h2>{slides[currentIndex].name}</h2>
-                  <p>{slides[currentIndex].text}</p>
+            <h1 id="title">Отзывы родителей</h1>
+            <div className="reviews">
+               <div className="container">
+                  <div className="img">
+                     <img src="img/avatar.png" alt="" />
+                  </div>
+                  <div className="main">
+                     <p>
+                        Мы искренне благодарны всем, кто поддерживает нашу
+                        работу и делится своими впечатлениями. Ваши отзывы
+                        вдохновляют нас и помогают улучшать наши программы. Вот
+                        что говорят родители, учителя и сами дети о наших
+                        инициативах:
+                     </p>
+                     <div className="carousel">
+                        {/* <button id="arrows" onClick={prevSlide}>
+                        <img
+                           src={`${process.env.PUBLIC_URL}/img/prev.png`}
+                           alt=""
+                        />
+                     </button> */}
+                        <div className="slide">
+                           {/* <h2>{slides[currentIndex].name}</h2> */}
+                           <p id="slider">{slides[currentIndex].text}</p>
+                        </div>
+                        <div className="buttons">
+                           <button id="arrows" onClick={prevSlide}>
+                              <img
+                                 src={`${process.env.PUBLIC_URL}/img/prev.png`}
+                                 alt=""
+                              />
+                           </button>
+                           <button id="arrows" onClick={nextSlide}>
+                              <img
+                                 src={`${process.env.PUBLIC_URL}/img/next.png`}
+                                 alt=""
+                              />
+                           </button>
+                        </div>
+                        {/* <button id="arrows" onClick={nextSlide}>
+                        <img
+                           src={`${process.env.PUBLIC_URL}/img/next.png`}
+                           alt=""
+                        />
+                     </button> */}
+                     </div>
+                  </div>
                </div>
-               <button id="arrows" onClick={nextSlide}>
-                  <img src={`${process.env.PUBLIC_URL}/img/next.png`} alt="" />
-               </button>
             </div>
          </div>
       </div>
