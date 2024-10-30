686227532bc7642a1ad6eda3332bfe32b82ec957
DonJuan <armvnov1ch@gmail.com>
2024-10-30 19:55:01 +0500

    photos with adaptive

--- src/components/Documents.jsx
+++ src/components/Documents.jsx
@@ -1,38 +1,29 @@
-import React from "react";
-const Documents = () => {
+import React from "react";const Documents = () => {
    return (
       <div id="documents">
          <div className="container">
-            <h3 id="title">Документы</h3>
+            <h1 id="title">Документы</h1>
             <div className="documents">
                <div className="document">
-                  <img
-                     src={`${process.env.PUBLIC_URL}/img/license.png`}
-                     alt=""
-                  />
-                  <h1>Лицензия</h1>
+                  <h1>
+                     (01) Лицензия
+                  </h1>
                   <a href="https://drive.google.com/file/d/1sp6BynVdL0-kFI-AefPtqJ9pFd_ZRWMb/view">
-                     Посмотреть
                   </a>
                </div>
                <div className="document">
-                  <img
-                     src={`${process.env.PUBLIC_URL}/img/regulation.png`}
-                     alt=""
-                  />
-                  <h1>Устав</h1>
+                  <h1>
+                     (02) Устав
+                  </h1>
                   <a href="https://drive.google.com/file/d/1tFYZmC5ZQRxTNoxnTbvrGmmQOgGJdYDD/view">
-                     Посмотреть
                   </a>
                </div>
                <div className="document">
-                  <img
-                     src={`${process.env.PUBLIC_URL}/img/scheduling.png`}
-                     alt=""
-                  />
-                  <h1>Расписание центров</h1>
+                  <h1>
+                     {" "}
+                     (03) Расписание центров
+                  </h1>
                   <a href="https://drive.google.com/file/d/1Ko6rZmJ85dYdNbqsQqRQab8Qi7oD_xTC/view">
-                     Посмотреть
                   </a>
                </div>
             </div>
