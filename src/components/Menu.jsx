0d5f820b95bc176cfb93b29a73a6eadeea9a2ce3
DonJuan <armvnov1ch@gmail.com>
2024-10-30 18:20:00 +0500

    nav

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -4,13 +4,19 @@ const Menu = () => {
       <div id="menu">
          <div className="container">
             <div className="nav">
-               <a href="/">О нас</a>
-               <a href="/correction">Коррекция</a>
-               <a href="/equipment">Оборудование</a>
-               <a href="/faq">FAQ</a>
-               <a id="donation" href="https://kaspi.kz/pay/OBalamaiAi?started_from=QR">
-                  Пожертвовать
-               </a>
+               <img src="/img/логотип.png" alt="" />
+               <div className="buttons">
+                  <a href="/">О нас</a>
+                  <a href="/correction">Коррекция</a>
+                  <a href="/equipment">Оборудование</a>
+                  <a href="/faq">FAQ</a>
+                  <a
+                     id="donation"
+                     href="https://kaspi.kz/pay/OBalamaiAi?started_from=QR"
+                  >
+                     Пожертвовать
+                  </a>
+               </div>
             </div>
          </div>
       </div>
