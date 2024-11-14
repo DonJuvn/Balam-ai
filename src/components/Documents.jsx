8304f093c6657cd08222d11c17f336c48b42769d
DonJuan <armvnov1ch@gmail.com>
2024-11-14 12:16:31 +0500

    ustav

--- src/components/Documents.jsx
+++ src/components/Documents.jsx
@@ -1,30 +1,21 @@
-import React from "react";const Documents = () => {
+import React from "react";
+const Documents = () => {
    return (
       <div id="documents">
          <div className="container">
             <h1 id="title">Документы</h1>
             <div className="documents">
                <div className="document">
-                  <h1>
-                     (01) Лицензия
-                  </h1>
-                  <a href="https://drive.google.com/file/d/1sp6BynVdL0-kFI-AefPtqJ9pFd_ZRWMb/view">
-                  </a>
+                  <h1>(01) Лицензия</h1>
+                  <a href="https://drive.google.com/file/d/1sp6BynVdL0-kFI-AefPtqJ9pFd_ZRWMb/view"></a>
                </div>
                <div className="document">
-                  <h1>
-                     (02) Устав
-                  </h1>
-                  <a href="https://drive.google.com/file/d/1tFYZmC5ZQRxTNoxnTbvrGmmQOgGJdYDD/view">
-                  </a>
+                  <h1>(02) Устав</h1>
+                  <a href="https://drive.google.com/file/d/1ajdC4_kvbNWNjI-fzget947FvWUmxFgc/view?usp=drivesdk"></a>
                </div>
                <div className="document">
-                  <h1>
-                     {" "}
-                     (03) Расписание центров
-                  </h1>
-                  <a href="https://drive.google.com/file/d/1Ko6rZmJ85dYdNbqsQqRQab8Qi7oD_xTC/view">
-                  </a>
+                  <h1> (03) Расписание центров</h1>
+                  <a href="https://drive.google.com/file/d/1Ko6rZmJ85dYdNbqsQqRQab8Qi7oD_xTC/view"></a>
                </div>
             </div>
          </div>
